a5b3946d590a559ff08302e557dad89de607e49e
Liis <liismarie@gmail.com>
2020-04-14 13:51:34 -0700

    Initial Commit

---
+++ MineSweeper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MineSweeper
+//
+//  Created by Liis on 14.04.2020.
+//  Copyright © 2020 Liis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

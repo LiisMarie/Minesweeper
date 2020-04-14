1ffb82962272c533c49edbe6ea0108db644eb176
Liis <liismarie@gmail.com>
2020-04-14 14:10:12 -0700

    added split view

---
+++ MineSweeper/RootSplitViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RootSplitViewController.swift
+//  MineSweeper
+//
+//  Created by Liis on 14.04.2020.
+//  Copyright © 2020 Liis. All rights reserved.
+//
+
+import UIKit
+
+class RootSplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        //self.delegate = self
+        //self.preferredDisplayMode = .allVisible
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+    /*
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+        return true
+    }*/
+
+}
